b06014392e4630efbd179843d32d9e4d296310d9
Ahmed Tawfiq <AhmedTawfiqM>
2021-04-29 13:08:10 +0200

    ImageCropper support URi

--- app/src/main/java/com/atdev/cropimageapp/MainActivity.kt
+++ app/src/main/java/com/atdev/cropimageapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.atdev.cropimageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.atdev.image_cropper.ImageCropper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStartCrop= findViewById<Button>(R.id.btnStartCrop)
+        btnStartCrop.setOnClickListener {
+            ImageCropper(this).start {
+                Toast.makeText(this,"result ${it.data}",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
